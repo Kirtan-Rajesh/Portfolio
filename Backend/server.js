8b88b756db4a0fcaac5ca8726811dd29883cf715
Kirtan Rajesh <96264432+Kirtan-Rajesh@users.noreply.github.com>
2024-06-19 15:30:36 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,13 +13,7 @@ const allowedOrigins = [
 
 // CORS configuration
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin:["https://portfolio-e2yw7qbld-kirtans-projects-444621e3.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
