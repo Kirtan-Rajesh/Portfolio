24b4e3906c3fbcd4946516dc96b5f9939f37892f
Kirtan Rajesh <96264432+Kirtan-Rajesh@users.noreply.github.com>
2024-06-19 15:55:41 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,31 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/about/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import ContactForm from "./components/contactform/ContactForm";
+
+const App = () => {
+  
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+    </section>
+      <section id="About"><Parallax type="about"/></section>
+      <section>
+        <About/>
+      </section>
+      <section id="Projects"><Parallax type="projects"/></section>
+      <section>
+        <Projects/>
+      </section>
+    <section id="Contact"> 
+        <ContactForm />
+    </section>
+  </div>;
+};
+
+export default App;
 
