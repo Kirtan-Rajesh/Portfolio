90dd2bb8233edf2e209782270a41c062718221c9
Kirtan Rajesh <96264432+Kirtan-Rajesh@users.noreply.github.com>
2024-06-19 16:52:02 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,9 +31,6 @@ app.use((req, res, next) => {
 app.use('/api/mails', mailroutes);
 
 // Root route for testing
-app.get('/', (req, res) => {
-    res.json("hello guys");
-});
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
