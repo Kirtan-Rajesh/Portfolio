6925967b5634f91cf87af078488bcb75a7b900e6
Kirtan Rajesh <96264432+Kirtan-Rajesh@users.noreply.github.com>
2024-06-19 15:42:40 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/about/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import About from "./components/about/About";
-import Projects from "./components/projects/projects";
+import Projects from "./components/projects/Projects";
 import ContactForm from "./components/contactform/ContactForm";
 
 const App = () => {
